4dd2a15e34475cc7bafa3689124bdda1b16df865
dong11hyun <eastmanin@gmail.com>
2026-01-21 20:13:13 +0900

    docs: 문서 수정

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Order
+from .models import Order, Restaurant, Rider
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'status', 'created_at') # 목록에 보여줄 칼럼들
+    list_display = ('id', 'status', 'restaurant', 'rider', 'created_at')
+    list_filter = ('status',)
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'address')
+
+@admin.register(Rider)
+class RiderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
